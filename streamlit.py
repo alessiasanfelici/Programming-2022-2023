2583dd5671724fc2a861e00637561e1384049bc1
alessiasanfelici <sanfelici.alessia@gmail.com>
2023-01-12 14:48:24 +0100

    Trying to make it work

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import streamlit as st
+import streamlit_tags as tags
+music_health_df = pd.read_csv("Music & Mental Health.csv")
+music_health_df.head()
