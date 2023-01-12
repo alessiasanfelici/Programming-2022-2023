2fd4af2a972f95fc4efc9cf1efaee7e99deeec03
alessiasanfelici <sanfelici.alessia@gmail.com>
2023-01-12 15:13:05 +0100

    Begin of exploration

--- streamlit.py
+++ streamlit.py
@@ -3,4 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 music_health_df = pd.read_csv("Music & Mental Health.csv")
-st.title("Ciao")
+st.title("Music & Mental Health Project")
+st.header("Exploration of the Dataset")
+st.write(music_health_df.head())
